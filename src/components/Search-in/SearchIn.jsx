03bf27495558bd1b433f33384d6e5afb1015fcf1
Destiny653 <fokundem.com@gmail.com>
2024-07-08 12:45:41 +0100

    potential commit

--- src/components/Search-in/SearchIn.jsx
+++ src/components/Search-in/SearchIn.jsx
@@ -21,7 +21,7 @@ export default function SearchIn() {
   return (
     <>
       <label className='search-block ' htmlFor="text">
-        <input value={searchValue} className='search-input w-full' type="text" name='text' autoComplete='true' onChange={(e) => { setSeacrchValue(e.target.value); forceUpdate() }} />
+        <input value={searchValue} className='search-input w-full text-black' type="text" name='text' autoComplete='true' onChange={(e) => { setSeacrchValue(e.target.value); forceUpdate() }} />
         <button onClick={() => { searchValue !== " " && navigation.push('/products'); forceUpdate() }} className='search-btn text-white relative' >Search
           <span onClick={() => setSeacrchValue(" ")} className='absolute -left-8   top-3 text-black text-sm rounded-full font-semibold bg-slate-100 px-1.5 py-0 hover:bg-amber-500 cursor-pointer'>x</span>
         </button>
